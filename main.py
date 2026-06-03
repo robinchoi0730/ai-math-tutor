70bbda14909f82a725e9112c61ac896ff8bf2c75
Robin Choi <robinchoi0730@gmail.com>
2026-06-03 01:53:10 -0700

    Connect FastAPI app to AI model

--- main.py
+++ main.py
@@ -1,1 +1,44 @@
-print("AI Math Tutor")
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY")
+)
+
+def ask_ai(question: str):
+
+    response = client.responses.create(
+        model="gpt-5",
+        input=question
+    )
+
+    return response.output_text
+
+# Connecting to Endpoint
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Question(BaseModel):
+    question: str
+
+
+@app.get("/")
+def home():
+    return {"message": "Welcome to AI Math Tutor"}
+
+
+@app.post("/ask")
+def ask_question(data: Question):
+
+    answer = ask_ai(data.question)
+
+    return {
+        "answer": answer
+    }
